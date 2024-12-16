9baedbbc304ee00fb25fdee3b36668a41f41ae4c
chaudhuree <chaudhuree00@gmail.com>
2024-12-17 03:30:46 +0600

    bug fix

--- clerk-role-based-authentication/src/app/api/webhooks/clerk/route.js
+++ clerk-role-based-authentication/src/app/api/webhooks/clerk/route.js
@@ -1,6 +1,6 @@
 import { Webhook } from 'svix'
 import { headers } from 'next/headers'
-import { WebhookEvent } from '@clerk/nextjs/server'
+import { clerkClient, WebhookEvent } from '@clerk/nextjs/server'
 import { createUser } from '@/actions/user.action'
 
 export async function POST(req) {
